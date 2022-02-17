cccff7bab0f2858ced7a47aa0c65ee2bd38b2342
mmathias <desenvolvedores.alumia@gmail.com>
2022-02-17 16:46:44 -0300

    Responsive slider show to 1

--- components/Slider/index.jsx
+++ components/Slider/index.jsx
@@ -29,8 +29,8 @@ export default class SimpleSlider extends Component {
         {
           breakpoint: 480,
           settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
+            slidesToShow: 1,
+            slidesToScroll: 1
             
           }
         }
