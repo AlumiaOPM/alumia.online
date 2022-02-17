7b5fde83da3cc0ebc0e77851fe73443bc411a24f
mmathias <desenvolvedores.alumia@gmail.com>
2022-02-17 16:34:49 -0300

    Adicionando Slider em parceiros

---
+++ components/Slider/index.jsx
@@ -0,0 +1,84 @@
+import React, { Component } from "react";
+import Slider from "react-slick";
+
+
+
+
+export default class SimpleSlider extends Component {
+  render() {
+    const settings = {
+      dots: true, 
+      arrows:false,
+      infinite: true,
+      autoplay: true,
+      speed: 300,
+      accessibility: true,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      className:'testeDivInterna',
+      className: "containerSlider",
+      responsive: [
+        
+        {
+          breakpoint: 700,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            initialSlide: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+            
+          }
+        }
+      ]
+    };
+    
+    return (
+      <>
+        <div style={{width:"80%", margin: "100px auto", paddingLeft:"4%"}} >
+          <div className="teste">
+            <h2 style={{ textAlign: "center", paddingBottom: "100px", fontSize: "2rem", fontWeight: "600", fontFamily: "Josefin Sans" }}>Nossos parceiros</h2>
+          </div>
+
+          <div style={{margin: "10px 0"}} >
+
+            <Slider {...settings}>
+              <div style={{paddingRight:"10px  !important"}} >
+                <img src="/parceiros/SaintLeo.png" alt="SaintLeo" title="SaintLeo" />
+              </div>
+              <div style={{paddingRight:"10px  !important"}} >
+                <img src="/parceiros/ESPM.png" alt="ESPM" title="ESPM" />
+              </div>
+              <div style={{paddingRight:"10px !important"}} >
+                <img src="/parceiros/sing.png" alt="Instituto Singularidades" title="Instituto Singularidades" />
+              </div>
+              <div style={{paddingRight:"10px !important"}} >
+                <img src="/parceiros/Fecap.png" alt="Fecap" title="Fecap" />
+              </div>
+              <div  style={{paddingRight:"10px !important"}} >
+                <img src="/parceiros/dmi.png" alt="Digital Marketing Institute" title="Digital Marketing Institute" />
+              </div>
+              <div style={{paddingRight:"10px !important"}} >
+                <img src="/parceiros/unifemm.png" alt="Unifemm" title="Unifemm" />
+              </div>
+              <div style={{paddingRight:"10px !important"}} >
+                <img src="/parceiros/fmp.png" alt="FMP" title="FMP" />
+              </div>
+              <div style={{paddingRight:"10px !important"}} >
+                <img src="/parceiros/ens.png" alt="ENS" title="ENS" />
+              </div>
+              <div style={{paddingRight:"10px !important"}} >
+                <img src="/parceiros/exc.png" alt="Executive Academy" title="Executive Academy" />
+              </div>
+            </Slider>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ import Depoimentos from '../components/Depoimentos';
 import Parceiros from '../components/Parceiros';
 import Popup from '../components/Popup';
 import Footer from '../components/Footer';
+import SimpleSlider from '../components/Slider/index'
 
 export default function Home() {
 
@@ -17,14 +18,19 @@ export default function Home() {
       <Head>
         <title>Alumia | A educação do futuro, hoje!</title>
         <link rel="icon" href="/favicon.png" />
+        <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
         <meta name="format-detection" content="telephone=no"></meta>
       </Head>
 
       <Header />
+
       <FirstSection />
+
       <PorQueAlumia />
       <SuaIESOnline />
-      <Parceiros />
+     {/*  <Parceiros /> */}
+      <SimpleSlider />
       <Depoimentos />
       <Popup />
       <Footer />
